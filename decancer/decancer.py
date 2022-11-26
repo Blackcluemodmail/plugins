d9a1dacabe4db15cd854317e9d582b41827dcfcc
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-11-26 17:16:32 +0530

    Update decancer.py

--- decancer/decancer.py
+++ decancer/decancer.py
@@ -408,5 +408,15 @@ class Decancer(commands.Cog):
         await ctx.send(f"I unfreezed <@{user.id}>")
 
 
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    async def userid(self, ctx):
+        thread = ctx.thread
+        if thread == None:
+            member = ctx.author
+        else:
+            member = thread.recipient
+        await ctx.send(f"{member.mention}'s ID is {member.id}")
+
 async def setup(bot):
     await bot.add_cog(Decancer(bot))
